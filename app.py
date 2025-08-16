c47f07b5d76f84ce839b6d3a485ee79409e1ad6a
RAGASUDHA B <ragasudha@student.tce.edu>
2025-08-16 19:08:15 +0530

    render and jinja2

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask,redirect,url_for,request
+from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route('/')
-def home():
-    return "hello"
-
-@app.route('/success/<name>')
-def success(name):
-    return 'wellcome %s' % name
-
-@app.route('/login',methods=['POST','GET'])
-def login():
-    if request.method == 'POST':
-        user=request.form.get('nm')
-        return redirect(url_for('success',name=user))
-    else:
-        user=request.args.get('nm')
-        return redirect(url_for('success',name=user))
+@app.route('/result')
+def result():
+    dict={'phy':50,'che':60,'maths':70}
+    return render_template('home.html',result=dict)
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug = True)
