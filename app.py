f67d19d7a0d259f80ad608d35210eb8c47dbb85b
RAGASUDHA B <ragasudha@student.tce.edu>
2025-08-16 12:24:03 +0530

    render_template,url_for,redirect

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'welcome'
+   return render_template('home.html')
 
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
 
+@app.route('/admin')
+def hello_admin():
+   return 'hello admin'
+
+@app.route('/user/<name>')
+def user(name):
+   if name=="admin":
+      return redirect(url_for("admin"))
+   else:
+      return redirect(url_for("admin"))
+   
 @app.route('/blog/<int:postID>')
 def show_blog(postID):
    return 'Blog Number %d' % postID
