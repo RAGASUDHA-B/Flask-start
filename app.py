5f1ba4b34438b2b45b5e006cae08ae8462c9e5ec
RAGASUDHA B <ragasudha@student.tce.edu>
2025-08-16 18:48:15 +0530

    using redirect url_for

--- app.py
+++ app.py
@@ -1,53 +1,21 @@
-from flask import Flask,render_template,url_for,redirect
-app = Flask(__name__)
-
-post=[
-   {
-      
-      'author':'corey schafer',
-      'title':'blog post 1',
-      'content':'first post content',
-      'date_posted':'april 20,2025'
-   },
-   {
-      
-      'author':'raga',
-      'title':'blog post 2',
-      'content':'post content',
-      'date_posted':'april 20'
-   }
-]
+from flask import Flask,redirect,url_for,request
+app=Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('home.html',post=post)
+    return "hello"
 
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
+@app.route('/success/<name>')
+def success(name):
+    return 'wellcome %s' % name
 
-@app.route('/about')
-def about():
-   return render_template('about.html')
-
-@app.route('/admin')
-def hello_admin():
-   return 'hello admin'
-
-@app.route('/user/<name>')
-def user(name):
-   if name=="admin":
-      return redirect(url_for("hello_admin"))
-   else:
-      return redirect(url_for("hello_admin",name=name))
-   
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-   return 'Blog Number %d' % postID
-
-@app.route('/rev/<float:revNo>')
-def revision(revNo):
-   return 'Revision Number %f' % revNo
-
-if __name__ == '__main__':
-   app.run(debug = True)
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        user=request.form.get('nm')
+        return redirect(url_for('success',name=user))
+    else:
+        user=request.args.get('nm')
+        return redirect(url_for('success',name=user))
+if __name__=='__main__':
+    app.run(debug=True)
