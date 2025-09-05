6fc5c9cae835260cfdf0db346e0009aa29d1b826
RAGASUDHA-B <sudha.balu.471@gmail.com>
2025-09-05 10:35:12 +0530

    changing and deleting

--- app.py
+++ app.py
@@ -1,15 +1,86 @@
-from flask import Flask,render_template
-from flask_scss import Scss
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
 
-#my app
-app=Flask(__name__)
+app = Flask(__name__)
+app.secret_key = 'flaskuser'
 
-#our main page
-@app.route("/")
-def index():
-    return render_template("index.html")
+# MySQL configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'MySQL@2409#'  # ✅ Your MySQL password
+app.config['MYSQL_DB'] = 'geeklogin'
 
+mysql = MySQL(app)
 
-if __name__=="__main__":
+# Home Route
+@app.route('/')
+def home():
+    if 'loggedin' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
+
+# Login Route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+
+        if account and check_password_hash(account['password'], password):
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return render_template('index.html', username=session['username'])
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('login.html', msg=msg)
+
+# Register Route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only letters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            hashed_password = generate_password_hash(password)
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, hashed_password, email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+
+# Logout Route
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+# Run the app
+if __name__ == '__main__':
     app.run(debug=True)
